16c2c2694c107ce805e176ad2f995b74b0a5b164
Qblack <blac2410@mylaurier.ca>
2015-06-07 17:48:59 -0400

    Added basic desired routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next) {
+  res.send('Here all all the users');
+});
+
+router.post('/', function(req, res, next) {
+    res.send('Creating a user');
+});
+
+
+router.get('/:id', function(req, res, next) {
+  res.send('Here is user '+req.params.id);
+});
+
+router.put('/:id', function(req, res, next) {
+    res.send('Updating user '+req.params.id);
+});
+
+
+router.delete('/:id', function(req, res, next) {
+    res.send('Deleting user '+req.params.id);
+});
+
+router.get('/:id/tasks', function(req, res, next) {
+    res.send('Here are user '+req.params.id+' tasks');
+});
+
+router.post('/:id/tasks', function(req, res, next) {
+    res.send('Adding a task to user '+req.params.id+' tasks');
+});
+
+router.get('/:id/tasks/:taskId', function(req, res, next) {
+    res.send('Here is user '+req.params.id+' task: '+req.params.taskId);
+});
+
+router.put('/:id/tasks/:taskId', function(req, res, next) {
+    res.send('Updating user '+req.params.id+' task: '+req.params.taskId);
+});
+
+router.delete('/:id/tasks/:taskId', function(req, res, next) {
+    res.send('Deleting user '+req.params.id+' task: '+req.params.taskId);
 });
 
 module.exports = router;
